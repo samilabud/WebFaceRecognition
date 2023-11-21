a12c57d1667d5d65b4984440822e46c411ad4a78
Samil Abud <samilabud@gmail.com>
2023-11-21 18:56:06 -0400

    Migrated api face recognition to railway.app

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,9 +1,8 @@
 export const enviroments = {
-    development:{
-        urlapi: "http://localhost:3000"
-    },
-    production:{
-        urlapi: "https://api-face-recognition.onrender.com"
-    }
-
-}
+  development: {
+    urlapi: "http://localhost:3000",
+  },
+  production: {
+    urlapi: "https://apifacerecognition-production.up.railway.app",
+  },
+};
