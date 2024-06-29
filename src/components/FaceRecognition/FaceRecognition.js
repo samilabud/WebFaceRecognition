23f51403b44af730c754d35f847f3edb5141ab48
Samil Abud <samilabud@gmail.com>
2024-06-29 12:00:03 -0400

    Improves the code, clearing logic

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import './FaceRecognition.css';
+import React from "react";
+import "./FaceRecognition.css";
 
-const FaceRecognition = ({imageURL,boxes}) =>{
-    let i = 0;
-    return (
-        <div className="center ma">
-            <div className="absolute mt2">
-                {imageURL!=="" ? 
-                    <img id="inputImage" alt="Imagen Analizada" src={imageURL} width="400"/>
-                   :
-                   <div></div> 
-                }
-                {
-                    boxes.map(box=>{
-                        i++;
-                    return <div key={i} className="faceBox" style={{top:box.topRow,left:box.leftCol, right:box.rightCol, bottom:box.bottomCol}}></div>
-                })}
-            </div>
-        </div>
-    )
-}
+const FaceRecognition = ({ imageURL, boxes }) => {
+  return (
+    <div className="center ma">
+      <div className="absolute mt2">
+        {imageURL !== "" ? (
+          <img
+            id="inputImage"
+            alt="Imagen Analizada"
+            src={imageURL}
+            width="400"
+          />
+        ) : (
+          <div></div>
+        )}
+        {boxes.map((box, index) => (
+          <div
+            key={index}
+            className="faceBox"
+            style={{
+              top: box.topRow,
+              left: box.leftCol,
+              right: box.rightCol,
+              bottom: box.bottomCol,
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default FaceRecognition;
