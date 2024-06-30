e7772d8b3d32c1b1be9bdcc91cfaeb88e9063bf1
Samil Abud <samilabud@gmail.com>
2024-06-30 16:30:45 -0400

    Fixes UI/UX of help instructions

--- src/App.js
+++ src/App.js
@@ -1,50 +1,49 @@
-import React, {useState, useCallback} from 'react';
-import Navigation from './components/Navigation/Navigation'
-import SignIn from './components/Signin/Signin'
-import Register from './components/Register/Register'
-import Logo from './components/Logo/Logo'
-import FaceRecognition from './components/FaceRecognition/FaceRecognition'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
-import Rank from './components/Rank/Rank'
-import './App.css';
-import 'tachyons';
+import React, { useState, useCallback } from "react";
+import Navigation from "./components/Navigation/Navigation";
+import SignIn from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
+import Logo from "./components/Logo/Logo";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import "./App.css";
+import "tachyons";
 import Particles from "react-tsparticles";
 import { loadSlim } from "tsparticles-slim";
-import {particleOptions} from './constants/particles';
-import {enviroments} from './constants/config';
-
+import { particleOptions } from "./constants/particles";
+import { enviroments } from "./constants/config";
 
 let urlapi = "";
-if(process.env.NODE_ENV.indexOf("development")>=0){
+if (process.env.NODE_ENV.indexOf("development") >= 0) {
   urlapi = enviroments.development.urlapi;
-}else{
+} else {
   urlapi = enviroments.production.urlapi;
 }
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [imageURL, setImageURL] = useState('');
+  const [input, setInput] = useState("");
+  const [imageURL, setImageURL] = useState("");
   const [facesBoxes, setFacesBoxes] = useState([]);
-  const [route, setRoute] = useState('signin');
+  const [route, setRoute] = useState("signin");
   const [isSignedin, setIsSignedin] = useState(false);
   const defaultUserData = {
-      id: '',
-      name: '',
-      email: '',
-      entries: 0,
-      joined: ''
-  }
+    id: "",
+    name: "",
+    email: "",
+    entries: 0,
+    joined: "",
+  };
   const [user, setUser] = useState(defaultUserData);
 
-  const loadUser = (data) =>{
+  const loadUser = (data) => {
     setUser({
-        id: data.id,
-        name: data.name,
-        email: data.email,
-        entries: data.entries,
-        joined: data.joinen
-    })
-  }
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joinen,
+    });
+  };
 
   const calculateFaceLocation = (data) => {
     const image = document.getElementById("inputImage");
@@ -53,109 +52,127 @@ const App = () => {
 
     //Almacenando todas las cajas
     let facesBoxes = [];
-    for(let region of data['outputs'][0]['data'].regions) {
-      facesBoxes.push(region.region_info.bounding_box)
+    for (let region of data["outputs"][0]["data"].regions) {
+      facesBoxes.push(region.region_info.bounding_box);
     }
     //Retornando todas las cajas
     let results = [];
-    for(let boxFace of facesBoxes) {
+    for (let boxFace of facesBoxes) {
       results.push({
         leftCol: boxFace.left_col * width,
         topRow: boxFace.top_row * height,
-        rightCol: width - (boxFace.right_col * width),
-        bottomCol: height - (boxFace.bottom_row * height)
-      })
+        rightCol: width - boxFace.right_col * width,
+        bottomCol: height - boxFace.bottom_row * height,
+      });
     }
     return results;
-
-  }
+  };
   const displayFaceBox = (facesBoxes) => {
     setFacesBoxes(facesBoxes);
-  }
+  };
 
   const onInputChange = (event) => {
     setInput(event.target.value);
-  }
-  const onHelpClicked = () =>{
-    document.getElementById("help").style.display="block";
-    document.getElementById("showHelp").style.display="none";
-  }
+  };
+  const onHelpClicked = () => {
+    document.getElementById("help").style.display = "block";
+    document.getElementById("showHelp").style.display = "none";
+  };
 
   const onImageSubmit = () => {
-    document.getElementById("help").style.display="none";
-    document.getElementById("showHelp").style.display="block";
+    document.getElementById("help").style.display = "none";
+    document.getElementById("showHelp").style.display = "block";
     setImageURL(input);
-    fetch(urlapi+"/imageurl",{
+    fetch(urlapi + "/imageurl", {
       method: "post",
-      headers: {"Content-Type":'application/json'},
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        input:input
-      })
+        input: input,
+      }),
     })
-      .then(response=>response.json())
-      .then(
-        (response) => {
-
-          if(response){
-            fetch(urlapi+"/image",{
-              method: "put",
-              headers: {"Content-Type":'application/json'},
-              body: JSON.stringify({
-                id:user.id
-              })
-            })
-            .then(data=>data.json())
-            .then(count=>setUser({...user, entries:count}))
-            .catch(err=>console.log("Ocurrio un error guardando los entries en el api local." + err))
-          }else{
-            console.log("not response")
-          }
-
-          displayFaceBox(calculateFaceLocation(response))
+      .then((response) => response.json())
+      .then((response) => {
+        if (response) {
+          fetch(urlapi + "/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: user.id,
+            }),
+          })
+            .then((data) => data.json())
+            .then((count) => setUser({ ...user, entries: count }))
+            .catch((err) =>
+              console.log(
+                "Ocurrio un error guardando los entries en el api local." + err
+              )
+            );
+        } else {
+          console.log("not response");
         }
-      )
-      .catch((error)=>console.log(error))
-  }
+
+        displayFaceBox(calculateFaceLocation(response));
+      })
+      .catch((error) => console.log(error));
+  };
   const onRouteChange = (route) => {
-    if(route==='signout'){
-      setInput('');
-      setImageURL('');
+    if (route === "signout") {
+      setInput("");
+      setImageURL("");
       setFacesBoxes([]);
-      setRoute('signin');
+      setRoute("signin");
       setIsSignedin(false);
       setUser(defaultUserData);
-      setRoute('signin');
-    }else if(route==='home')
-    setIsSignedin(true)
+      setRoute("signin");
+    } else if (route === "home") setIsSignedin(true);
 
     setRoute(route);
-  }
+  };
 
-  const particlesInit = useCallback(async engine => {
+  const particlesInit = useCallback(async (engine) => {
     await loadSlim(engine);
   }, []);
 
   return (
     <div className="App">
-    <Particles id="tsparticles" init={particlesInit} options={particleOptions} className="particle"/>
-    <Navigation  onRouteChange={onRouteChange} isSignedin={isSignedin} Route={route} />
-      { 
-        route === 'home'?
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        options={particleOptions}
+        className="particle"
+      />
+      <Navigation
+        onRouteChange={onRouteChange}
+        isSignedin={isSignedin}
+        Route={route}
+      />
+      {route === "home" ? (
         <div>
-            <Logo />
-            <Rank user = {user}/>
-            <ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onImageSubmit} onHelpClicked={onHelpClicked} />
-            <FaceRecognition boxes={facesBoxes} imageURL={imageURL} />
+          <Logo />
+          <Rank user={user} />
+          <ImageLinkForm
+            onInputChange={onInputChange}
+            onButtonSubmit={onImageSubmit}
+            onHelpClicked={onHelpClicked}
+            setInput={setInput}
+          />
+          <FaceRecognition boxes={facesBoxes} imageURL={imageURL} />
         </div>
-        :(route==='signin'?
-          <SignIn onRouteChange={onRouteChange} loadUser={loadUser} urlApi={urlapi}/>
-          :
-          <Register onRouteChange={onRouteChange} loadUser={loadUser} urlApi={urlapi}/>
-        )           
-      }
+      ) : route === "signin" ? (
+        <SignIn
+          onRouteChange={onRouteChange}
+          loadUser={loadUser}
+          urlApi={urlapi}
+        />
+      ) : (
+        <Register
+          onRouteChange={onRouteChange}
+          loadUser={loadUser}
+          urlApi={urlapi}
+        />
+      )}
     </div>
   );
-    
-}
+};
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,30 +1,98 @@
-import React from 'react';
-import './ImageLinkForm.css'
+import React from "react";
+import "./ImageLinkForm.css";
 
-const ImageLinkForm = ({onInputChange, onButtonSubmit, onHelpClicked}) => {
-    return (
-        <div>
-            <p className="f3">
-                {'Este cerebro magico detectara los rostros de las personas en cualquier fotografia.'}
-            </p>
-            <div className="center form pa4 br3 shadow-5 ">
-                <div className="center w-100">
-                    <input type='text' className='f4 pa2 w-70 center' onChange={onInputChange}></input>
-                    <button onClick={onButtonSubmit} className='w-30 grow f4 link ph3 pv2 dib white bg-light-blue center'>Detectar</button>
-                </div>
-            </div>
-            <div className="">
-                <button id="showHelp" onClick={onHelpClicked} className="f5 center">¿Ayuda?</button> <br />
-                <div id="help" className="center">
-                    {'Ej. copie y pegue en el campo de texto el siguiente enlace: '} 
-                    <br />
-                    {'https://i2-prod.mirror.co.uk/incoming/article14334083.ece/ALTERNATES/s615/3_Beautiful-girl-with-a-gentle-smile.jpg'}<br />
-                    {'https://merriam-webster.com/assets/mw/images/article/art-wap-article-main/family-of-four-7101-90ebbf35ea8e9b9d264dbb2ab724bc4e@1x.jpg'}
-                </div>
-            </div>
+const ImageLinkForm = ({
+  onInputChange,
+  onButtonSubmit,
+  onHelpClicked,
+  setInput,
+}) => {
+  const links = [
+    {
+      href: "https://i2-prod.mirror.co.uk/incoming/article14334083.ece/ALTERNATES/s615/3_Beautiful-girl-with-a-gentle-smile.jpg",
+      text: "One Face Girl",
+    },
+    {
+      href: "https://merriam-webster.com/assets/mw/images/article/art-wap-article-main/family-of-four-7101-90ebbf35ea8e9b9d264dbb2ab724bc4e@1x.jpg",
+      text: "Multiple Faces",
+    },
+  ];
+  const handleExampleClick = (url) => {
+    document.getElementById("inputUrl").value = url;
+    setInput(url);
+  };
+
+  const onHideHelpClicked = () => {
+    document.getElementById("help").style.display = "none";
+    document.getElementById("showHelp").style.display = "block";
+  };
+
+  return (
+    <div>
+      <p className="f3">
+        {
+          "Este cerebro magico detectara los rostros de las personas en cualquier fotografia."
+        }
+      </p>
+      <div className="center form pa4 br3 shadow-5 ">
+        <div className="center w-100">
+          <input
+            id="inputUrl"
+            type="text"
+            className="f4 pa2 w-70 center"
+            onChange={onInputChange}
+          ></input>
+          <button
+            onClick={onButtonSubmit}
+            className="w-30 grow f4 link ph3 pv2 dib white bg-light-blue center"
+          >
+            Detect
+          </button>
         </div>
-        
-    )
-}
+      </div>
+      <div className="">
+        <button
+          id="showHelp"
+          onClick={onHelpClicked}
+          className="f5 center"
+          style={{ marginTop: "2px" }}
+        >
+          Help
+        </button>
+        <br />
+        <div
+          id="help"
+          className="center"
+          style={{
+            border: "1px solid black",
+            width: "235px",
+            textAlign: "center",
+          }}
+        >
+          <h5>Click on these examples to paste the URL:</h5>
+          <ul style={{ listStyleType: "none" }}>
+            {links.map((link, index) => (
+              <li
+                key={index}
+                onClick={() => handleExampleClick(link.href)}
+                className="li-link"
+              >
+                {link.text}
+              </li>
+            ))}
+          </ul>
+          <button
+            id="showHelp"
+            className="f5 center"
+            onClick={onHideHelpClicked}
+            style={{ marginBottom: "2px" }}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ImageLinkForm;
